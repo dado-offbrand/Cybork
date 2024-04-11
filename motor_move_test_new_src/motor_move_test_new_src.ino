bf085cd543d6bf24d682fee97e2e28c026686d4a
Ben <82716251+dado-offbrand@users.noreply.github.com>
2024-04-10 20:44:21 -0400

    Code is now functional.

--- motor_move_test_new_src/motor_move_test_new_src.ino
+++ motor_move_test_new_src/motor_move_test_new_src.ino
@@ -12,7 +12,7 @@
 #define INC_POSITION  20.0 //     S,    UL,   LL
 std::vector<uint8_t> motor_ids = {0x7F, 0x7E, 0x7D};
 std::vector<float> positions = {0.0f, 0.0f, 0.0f};
-bool bounce = false;
+int current_index = 2; // default start on LL
 
 CybergearController controller = CybergearController(0x00);
 #ifdef USE_ESP32_CAN
@@ -24,39 +24,43 @@ CybergearCanInterfaceMcp interface;
 void setup() {
   M5.begin();
 
-  M5.Lcd.print("Init motors ... ");
+  M5.Lcd.print("Setting up stuff ... ");
   interface.init(12, 15);
   controller.init(motor_ids, MODE_POSITION, &interface);
   controller.enable_motors();
-  M5.Lcd.println("done");
 
-  //M5.Lcd.print("configuring settings ... ");
-  //M5.Lcd.println("done");
+  controller.send_position_command(motor_ids, positions);
 
-  M5.Lcd.print("safety wait period ... ");
   delay(200);
   M5.Lcd.println("done");
 }
 
 void loop() {
   M5.update();
-
-  M5.Lcd.print("calculate rotation position ... ");
+  
   // DO NOT GIVE 0x7F (p[0]) OR 0x7E (p[1]) UNLIMITED ROTATION.
-
-  if (bounce) {
-    positions[2] += INC_POSITION / 180.0f * M_PI;
-  } else {
-    positions[2] -= INC_POSITION / 180.0f * M_PI;
+  if (M5.BtnA.wasPressed()) {
+    positions[current_index] -= 10.0f / 180.0f * M_PI;
+  } else if (M5.BtnB.wasPressed()) {
+    current_index++;
+    if (current_index > 2) {
+      current_index = 0;  
+    }
+  } else if (M5.BtnC.wasPressed()) {
+      positions[current_index] +=  10.0f / 180.0f * M_PI;
   }
-  bounce != bounce;
-  
-  M5.Lcd.println("done");
 
-  M5.Lcd.print("send command position ... ");
   //controller.send_position_command(motor_ids, positions);
-  controller.send_position_command(motor_ids[2], positions[2]);
-  M5.Lcd.println("done");
-  
+  controller.send_position_command(motor_ids[current_index], positions[current_index]);
+
+  draw_screen();
   delay(200);
+}
+
+void draw_screen() {
+  M5.Lcd.print("Current motor: ");
+  M5.Lcd.println(current_index);
+
+  M5.Lcd.print("Current position: ");
+  M5.Lcd.println(positions[current_index]);
 }
