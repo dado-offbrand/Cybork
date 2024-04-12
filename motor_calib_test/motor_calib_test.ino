542c3fe405f5c92bdff43c6a63cdea02c08b0b3d
Ben <82716251+dado-offbrand@users.noreply.github.com>
2024-04-12 11:46:12 -0400

    Updated to use driver instead of controller

--- motor_calib_test/motor_calib_test.ino
+++ motor_calib_test/motor_calib_test.ino
@@ -1,20 +1,22 @@
 #include <Arduino.h>
 #include <math.h>
 #include <M5Stack.h>
-#include "cybergear_controller.hh"
+#include "cybergear_driver.hh"
 #include <vector>
 #ifdef USE_ESP32_CAN
 #include "cybergear_can_interface_esp32.hh"
 #else
 #include "cybergear_can_interface_mcp.hh"
 #endif
-//                                0 S   1 uL  2 LL
-std::vector<uint8_t> motor_ids = {0x7F, 0x7E, 0x7D};
-std::vector<float> positions = {0.0f, 0.0f, 0.0f};
-std::vector<MotorStatus> motor_stats;
+
+#define SLOW_SPEED 0.5
+#define NORMAL_SPEED 5
+
+float position = 0.0f;
+MotorStatus status;
 int current_index = 1;
 
-CybergearController controller = CybergearController(0x00);
+CybergearDriver driver = CybergearDriver(0x00, 0x7E);
 #ifdef USE_ESP32_CAN
 CybergearCanInterfaceEsp32 interface;
 #else
@@ -28,42 +30,44 @@ void setup() {
 
   M5.Lcd.print("Init systems ... ");
   interface.init(12, 15);
-  controller.init(motor_ids, MODE_POSITION, &interface);
-  controller.enable_motor(motor_ids[current_index], MODE_POSITION);
-  controller.set_speed_limit(motor_ids[current_index], 0.5f);
+  driver.init(&interface);
+  driver.init_motor(MODE_POSITION);
+  driver.set_limit_speed(SLOW_SPEED);
+  driver.enable_motor();
   M5.Lcd.println("done");
 
   M5.Lcd.print("Setting pos: 0f @ 0.5rad ...");
-  controller.send_position_command(motor_ids[current_index], 0.0f);
-  delay(4000);
-  M5.Lcd.println("done")
-
-  M5.Lcd.print("Setting pos: 0.9f @ 0.5rad ...");
-  controller.send_position_command(motor_ids[current_index], 0.9f);
+  driver.set_position_ref(0.0f);
   delay(4000);
   M5.Lcd.println("done");
 
-  controller.set_mech_position_to_zero(motor_ids[current_index]);
+  M5.Lcd.print("Setting pos: 0.9f @ 0.5rad ...");
+  driver.set_position_ref(0.9f);
+  delay(4000);
+  M5.Lcd.println("done");
+
+  driver.set_mech_position_to_zero();
   M5.Lcd.println("Set mech pos to zero");
 
-  controller.set_speed_limit(motor_ids[current_index, 1f);
+  driver.set_limit_speed(NORMAL_SPEED);
   M5.Lcd.println("Set speed limit back to 1rad");
 }
 
 void loop() {
   M5.update();
 
-  (M5.BtnA.wasPressed()) {
-    positions[current_index] -= 20.0f / 180.0f * M_PI;
-    controller.send_position_command(motor_ids[current_index], positions[current_index]);  
+  if (M5.BtnA.wasPressed()) {
+    position -= 20.0f / 180.0f * M_PI;
+    driver.set_position_ref(position); 
   } else if (M5.BtnB.wasPressed()) {
-    controller.set_speed_limit(motor_ids[current_index], 0.5f);
-    controller.send_position_command(motor_ids[current_index], 0f)]
+    driver.set_limit_speed(SLOW_SPEED);
+    driver.set_position_ref(0.0f); 
     delay(4000);
-    controller.set_speed_limit(motor_ids[current_index], 1f);
-  } else  if (M5.BtnC.wasPressed()) {
-    positions[current_index] += 20.0f / 180.0f * M_PI;
-    controller.send_position_command(motor_ids[current_index], positions[current_index]);  
+    driver.set_limit_speed(NORMAL_SPEED);
+    position = 0.0f;
+  } else if (M5.BtnC.wasPressed()) {
+    position += 20.0f / 180.0f * M_PI;
+    driver.set_position_ref(position); 
   }
 
   delay(200);
