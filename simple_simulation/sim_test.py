ba97d3b0a07ad081531465160c0dd4285349cb02
Ben <82716251+dado-offbrand@users.noreply.github.com>
2024-09-28 18:16:50 -0400

    Messed around with physics sim again

--- simple_simulation/sim_test.py
+++ simple_simulation/sim_test.py
@@ -7,23 +7,17 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #"optionally"? what does that mean
 
 # set up test environment
-p.setGravity(0, 0, -10)
+p.setGravity(0, 0, -9.8)
 planeID = p.loadURDF("plane.urdf")
 
 # set up test object
 startPos = [0, 0, 1] #vec3?
 startOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("r2d2.urdf",startPos, startOrientation)
-# how do i make a URDF file.....
+robotId = p.loadURDF("example_quad.urdf", startPos, startOrientation)
 
 # begin simulation?
-for i in range (10000):
+while True:
     p.stepSimulation()
-    time.sleep(1./240.)
-
-# print results
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-print(cubePos,cubeOrn)
 
 # disconnect simulation
-p.disconnect()
+#p.disconnect()
